f919ad59d0bf5bf74c610722a6ba61f3bb4ac664
CHENG Yipeng <s2092119@ed.ac.uk>
2021-08-25 16:16:14 +0100

    Added test for prediction with continuous bart.

--- tests/testthat/test_predictmprmodel.R
+++ tests/testthat/test_predictmprmodel.R
@@ -6,6 +6,11 @@ dummyTestY <- rbinom(100, 1, 0.2)
 
 # Test continuous models
 continuousglmnetModel <- fitMPRModel(type = 'continuous', method = 'glmnet', trainXs = dummyTrainXs, trainY = rnorm(100), alpha = 0.5)
-testPredictions <- predictMPRModel(continuousglmnetModel, dummyTestXs, s = continuousglmnetModel$model$lambda[[1]])
+continuousglmnettestPredictions <- predictMPRModel(continuousglmnetModel, dummyTestXs, s = continuousglmnetModel$model$lambda[[1]])
 
-expect_equal(length(testPredictions), nrow(dummyTestXs))
+expect_equal(length(continuousglmnettestPredictions), nrow(dummyTestXs))
+
+continuousbartModel <- fitMPRModel(type = 'continuous', method = 'bart', trainXs = dummyTrainXs, trainY = rnorm(100))
+continuousbarttestPredictions <- predictMPRModel(continuousbartModel, dummyTestXs)
+
+expect_equal(length(continuousbarttestPredictions), nrow(dummyTestXs))
