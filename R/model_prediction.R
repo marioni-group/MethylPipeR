3b795e6b1a207e1300cf98da23833002653f4e6e
CHENG Yipeng <s2092119@ed.ac.uk>
2021-09-09 11:49:01 +0100

    Added binary bart and rf prediction. median function is now passed to apply instead of 'median' string in continuous bart prediction.

--- R/model_prediction.R
+++ R/model_prediction.R
@@ -10,7 +10,7 @@ predictMPRModel <- function(model, data, bartMeanOrMedian = 'mean', ...) {
   checkNA(data)
   checkMatrixOrDF(data)
   if (S3Class(model) != 'MPRModel') {
-    stop('predictMPRModel: model must be of class mprModel')
+    stop('predictMPRModel: model must be of class MPRModel')
   }
   type <- model$modelType
   method <- model$modelMethod
@@ -21,10 +21,16 @@ predictMPRModel <- function(model, data, bartMeanOrMedian = 'mean', ...) {
         as.numeric(predict(model$model, data, ...))
       },
       'bart' = function() {
-        # TODO: implement
+        bartPredictResult <- predict(model$model, newdata = data, ...)
+        if (bartMeanOrMedian == 'mean') {
+          apply(bartPredictResult$prob.test, 2, mean)
+        } else if (bartMeanOrMedian == 'median') {
+          apply(bartPredictResult$prob.test, 2, median)
+        }
       },
       'rf' = function() {
-        # TODO: implement
+        rfPredictResult <- predict(model$model, newdata = data, ...)
+        rfPredictResult
       }
     ),
     'survival' = list(
@@ -47,7 +53,7 @@ predictMPRModel <- function(model, data, bartMeanOrMedian = 'mean', ...) {
         if (bartMeanOrMedian == 'mean') {
           apply(bartPredictResult, 2, mean)
         } else if (bartMeanOrMedian == 'median') {
-          apply(bartPredictResult, 2, 'median')
+          apply(bartPredictResult, 2, median)
         }
       },
       'rf' = function() {
